a8cc5a9843a242f816754cd77af994d8821c798d
Rafael del Rio <rafaelgdelrio@gmail.com>
2024-01-24 15:02:42 -0300

    Navigation component setup and add fragment layouts

---
+++ app/src/main/java/com/rafael/contact_list/AddContactFragment.kt
@@ -0,0 +1,34 @@
+package com.rafael.contact_list
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
+import com.rafael.contact_list.databinding.FragmentAddContactBinding
+
+class AddContactFragment : Fragment() {
+    private var _binding: FragmentAddContactBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        _binding = FragmentAddContactBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding.topAppBar.setNavigationOnClickListener {
+            findNavController().navigateUp()
+        }
+    }
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+
+}
--- app/src/main/java/com/rafael/contact_list/MainActivity.kt
+++ app/src/main/java/com/rafael/contact_list/MainActivity.kt
@@ -2,10 +2,19 @@ package com.rafael.contact_list
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var navController: NavController
     override fun onCreate(savedInstanceState: Bundle?) {
+        enableEdgeToEdge()
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+        val navController = navHostFragment.navController
     }
 }
