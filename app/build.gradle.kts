8a40e53b76b1e283a5b5b3e703ef91cde8c82574
Rafael del Rio <rafaelgdelrio@gmail.com>
2024-01-25 18:01:42 -0300

    Fix Navigation dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     id("com.android.application")
     id("org.jetbrains.kotlin.android")
+    id("androidx.navigation.safeargs.kotlin")
     id("com.google.devtools.ksp") version "1.9.21-1.0.15"
 }
 
